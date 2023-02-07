c7eb95eec8d651382d71bae4213d8ed0bd8932a6
Xin Chen <xin.xi.chen@oracle.com>
2023-02-06 20:37:04 -0800

    improved filter to only show US based stock market, improved feature to include response from last X days

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ class InsiderUpdates:
 
     def filter_by_value(self, data, value_lower_bound: float):
         filtered_data = []
+        us_exchange = ['NYSE', 'NAS']
         for item in data:
             # Only include transactions with type "P" (Purchase)
-            if item['type'] != "S":
+            if item['type'] != "S" and item['exchange'] in us_exchange:
                 # Calculate the value of the transaction
                 trans_value = item['cost'] * item['trans_share']
                 # Only include transactions with value greater than the lower bound
@@ -38,16 +39,29 @@ class InsiderUpdates:
             data = response.json()
             return data
 
+def combine_responses(responses):
+    combined_response = []
+    for response in responses:
+        combined_response += response
+    return combined_response
 
 def main():
     with open("api_token.txt", "r") as f:
         api_token = f.read().strip()
 
     insider_updates = InsiderUpdates(api_token, "AAPL")
-    data = insider_updates.get_data()
-    filtered_data = insider_updates.filter_by_value(data, 1000000)
-    print(f"Number of transactions found: {len(filtered_data)}")
-    print(json.dumps(filtered_data, indent=4))
+
+    date = datetime.now().date()
+    filtered_data_list = []
+    for i in range(5):
+        data = insider_updates.get_data(date.strftime('%Y-%m-%d'))
+        filtered_data = insider_updates.filter_by_value(data, 200000)
+        filtered_data_list.append(filtered_data)
+        date -= timedelta(days=1)
+
+    combined_response = combine_responses(filtered_data_list)
+    print(f"Number of transactions found: {len(combined_response)}")
+    print(json.dumps(combined_response, indent=4))
 
 
 if __name__ == '__main__':
